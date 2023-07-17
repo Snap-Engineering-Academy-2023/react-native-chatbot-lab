02cec042cb9221ca4092797b511b62c56570bd0d
Kalvin Sevillano <kalvinsevillano@Kalvins-MacBook-Pro.local>
2023-07-17 13:41:42 -0700

    Working on checkpoint 5 (hangman)

--- App.js
+++ App.js
@@ -23,8 +23,6 @@ export default function App() {
 
   const addNewMessage = (newMessages) => {
     setMessages((previousMessages) => {
-      // console.log("PREVIOUS MESSAGES:", previousMessages);
-      // console.log("NEW MESSAGE:", newMessages);
       return GiftedChat.append(previousMessages, newMessages);
     });
   };
@@ -40,23 +38,77 @@ export default function App() {
     ]);
   };
 
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+  const [index, setIndex] = useState(0);
+  const [start, setStart] = useState(false);
+  const [guessedMessage, setGuessedMessage] = useState("");
+  const phrases = [
+    {
+      phrase: "why did the chicken cross the road?",
+    },
+    {
+      phrase: "have you had any water today",
+    },
+    {
+      phrase: "this took you forever to figure out",
+    },
+  ];
+
+  const [partialPhrase, setPartialPhrase] = useState("");
+  const phraseMap = new Map();
+
+  const incorrectMessage = "No!!!!!!! TRY again";
+  const successMessage = ["ok i see you", "bruh nice", "think your smart eh?"];
+
   const respondToUser = (userMessages) => {
-    console.log("User message text:", userMessages[0].text);
+    let newResponse = userMessages[0].text;
+    setResponse(newResponse);
+    console.log(response);
+    if (newResponse == "Yes") {
+      setStart(true);
+      addBotMessage(
+        "Begin by guessing the right charcters to reveal the phrase! GO!"
+      );
+      // set the secret word
+      setPartialPhrase(phrases[0].phrase);
+      // sent partially guessed word (which is "_____ _____ _____ _____")
+      let hiddenMessage = "";
+      for (let i = 0; i < phrases[0].phrase.length; i++) {
+        if (phrases[0].phrase[i] != " ") {
+          hiddenMessage += "_ ";
+        } else {
+          hiddenMessage += "    ";
+        }
+      }
+      addBotMessage(hiddenMessage);
+    } else if (newResponse != "Yes" && start != true) {
+      addBotMessage("No rush. When you're ready, type 'Yes'");
+    }
 
-    // Simple chatbot logic (aka Checkpoint 2 onwards) here!
+    // if game has started and user's message is one letter long
+    if (start && newResponse.length == 1) {
+      // process that letter as an answer for hangman (call hangman with letter guess)
+    }
+  };
 
-    addBotMessage("I am da response!");
+  const hangMan = (guessedLetter) => {
+    // check the guessed letter against secret word
+    // update the partially guessed word
+    // send user the updated partially guessed word
   };
 
   const onSend = useCallback((messages = []) => {
     addNewMessage(messages);
-    // setTimeout(() => respondToUser(messages), 1000);
   }, []);
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages) => onSend(messages)}
+      onSend={(messages) => {
+        onSend(messages);
+        setTimeout(() => respondToUser(messages), 1000);
+      }}
       user={{
         _id: 1,
         name: "Baker",
